69e4864e4ed4e033343b328be1563899940b74c3
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-12-12 21:04:24 +0530

    Display Databases and Using Database

--- src/main/java/queries.sql
+++ src/main/java/queries.sql
@@ -1,3 +1,21 @@
 --UC1
 mysql> create database payroll_service;
 Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| AddressBook        |
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sys                |
++--------------------+
+
+mysql> use payroll_service;
+Database changed
+
+
+
